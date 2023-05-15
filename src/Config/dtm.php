52022f2d629423ab51e910237d5767e7c619fdd6
brown_sweet <455764041@qq.com>
2023-05-15 15:13:53 +0800

    修改了依赖

---
+++ src/Config/dtm.php
@@ -0,0 +1,21 @@
+<?php
+
+use Dtm\Constants\DbType;
+use Dtm\Constants\Protocol;
+
+return [
+    'dtm'=>[
+        'protocol' => Protocol::HTTP,
+        'server' => '82.157.58.76',
+        'port' => [
+            'http' => 36789,
+            'grpc' => 36790,
+        ],
+        'barrier' => [
+            'db' => [
+                'type' => DbType::MySQL,
+            ],
+            'apply' => [],
+        ],
+    ]
+];
